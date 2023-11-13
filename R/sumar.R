cdb691f79632d565fd84a59d2b1c11c2d234e36b
FenrirOfMensis <moritzkeller20@gmail.com>
2023-11-13 13:59:48 +0100

    v2

--- R/sumar.R
+++ R/sumar.R
@@ -4,7 +4,7 @@
 #' @param y Second element to sum.
 #' @param negative TRUE if the elements to subtract.
 #'
-#' @return The sum of two elements.
+#' @return The Sum of two elements.
 #' @export
 #'
 #' @examples
